fcb39c3d2e584601c05f2decc68984b12a764f70
VaL <91805686+Valqueen1@users.noreply.github.com>
2022-11-18 11:18:44 -0300

    Termino el main

--- GUI Test/src/Main.java
+++ GUI Test/src/Main.java
@@ -4,7 +4,24 @@ import static java.lang.Thread.sleep;
 public class Main {
     public static void main(String[] args) throws InterruptedException {
         Ventana ventana = new Ventana();
+        while(true){
+            if(ventana.trabajo.isIniciar()){
+                while(!ventana.trabajo.finTiempo() && ventana.trabajo.isIniciar()){
+                    ventana.cambiarTiempo();
+                    ventana.trabajo.reducirTiempo();
+                    sleep(1000);
+                }
+            }
+            if(ventana.descanso.isIniciar()){
+                while(!ventana.descanso.finTiempo() && ventana.descanso.isIniciar()){
+                    ventana.cambiarTiempo();
+                    ventana.descanso.reducirTiempo();
+                    sleep(1000);
+                }
+            }
+            ventana.cambiarTiempo();
+            ventana.finalTiempo();
 
-
+        }
     }
 }
